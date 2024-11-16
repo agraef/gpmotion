a55b5387a708ff6e4fc0dd2e18663ce042e627b3
Albert Graef <aggraef@gmail.com>
2024-11-16 08:41:47 +0100

    Add GamepadMotion* member, initialization and finalization.

--- gpmotion.c
+++ gpmotion.c
@@ -1,29 +1,41 @@
 
 #include "m_pd.h"
+#include "motion.hpp"
+
+// TODO: add list outlet; messages for start/stop/reset calibration; process
+// IMU data; get gravity, local, world, and player vectors
 
 static t_class *gpmotion_class;
 
 typedef struct _gpmotion {
     t_object  x_obj;
+    GamepadMotion *gp;
 } t_gpmotion;
 
 void gpmotion_bang(t_gpmotion *x)
 {
-    post("Hello world !!");
+    post("gpmotion %p", x->gp);
 }
 
 void *gpmotion_new(void)
 {
     t_gpmotion *x = (t_gpmotion *)pd_new(gpmotion_class);
+    x->gp = gamepad_motion_init();
 
     return (void *)x;
+}
+
+void gpmotion_free(t_gpmotion *x)
+{
+    gamepad_motion_fini(x->gp);
 }
 
 void gpmotion_setup(void)
 {
     gpmotion_class = class_new(gensym("gpmotion"),
                                (t_newmethod)gpmotion_new,
-                               0, sizeof(t_gpmotion),
+                               (t_method)gpmotion_free,
+                               sizeof(t_gpmotion),
                                CLASS_DEFAULT, 0);
 
     class_addbang(gpmotion_class, gpmotion_bang);
